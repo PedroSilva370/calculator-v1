59791005922cad233c1b8fcb725e416af1ce2db3
Pedro Silva <gsilvapedro370@gmail.com>
2026-02-23 11:41:35 -0300

    Calculator v1
    This is a simple Python calculator that uses +, -, *, and /

---
+++ calculator v1.py
@@ -0,0 +1,11 @@
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter another number: '))
+signal = input('The calculation is +, -, *, or /: ')
+if signal == '+':
+    print(n1 + n2)
+if signal == '-':
+  print(n1 - n2)
+if signal == '*':
+    print(n1 * n2)
+if signal == '/':
+    print(n1 / n2)
